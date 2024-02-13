e7657bc4600c3b999b536c6bce07378fb23bea73
vitorlana45 <vitoresmerio00@gmail.com>
2024-02-13 17:00:10 -0300

    User insert

--- src/main/java/com/project/lana/course/resources/UserResource.java
+++ src/main/java/com/project/lana/course/resources/UserResource.java
@@ -5,11 +5,10 @@ import com.project.lana.course.Service.UserService;
 import com.project.lana.course.entities.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 
@@ -32,4 +31,12 @@ public class UserResource {
         User obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
     }
+
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User obj) {
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
+    }
+
 }
